94da960b14143d6b30ff42a89685dd51aa40565b
sunprizrak <sunprizrak@gmail.com>
2023-05-21 00:43:19 +0300

    ...

--- trucking/init-letsencrypt.sh
+++ trucking/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(5-180-183-166.cloud-xip.com www.5-180-183-166.cloud-xip.com)
+domains=(sowo.pro www.sowo.pro)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="6574152@gmail.com" # Adding a valid address is strongly recommended
